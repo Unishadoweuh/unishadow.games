f12a2f237eb6c27e8e40968156452b715aa1236d
Unishadow <unishadow@etik.com>
2024-07-02 11:43:36 +0200

    up

--- components/Project.vue
+++ components/Project.vue
@@ -74,7 +74,7 @@ export default {
 
             return {
                 icon: ['fas', 'link'],
-                text: 'Open Link',
+                text: 'Ouvrir le lien',
             }
         }
     },
